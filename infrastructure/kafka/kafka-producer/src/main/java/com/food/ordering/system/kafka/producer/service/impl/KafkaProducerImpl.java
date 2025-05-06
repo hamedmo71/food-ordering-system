3dc2af4c5c3bbf62392596714a34bcdde115590c
Hamed.Mohammadian <hamed.mohammadian2010@gmail.com>
2025-05-06 10:23:28 +0330

    Implementing the foundation of messaging with kafka and Avro serialization

---
+++ infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/service/impl/KafkaProducerImpl.java
@@ -0,0 +1,39 @@
+package com.food.ordering.system.kafka.producer.service.impl;
+
+import com.food.ordering.system.kafka.producer.exception.KafkaProducerException;
+import com.food.ordering.system.kafka.producer.service.KafkaProducer;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.springframework.kafka.KafkaException;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.BiConsumer;
+
+@Component
+@Slf4j
+public class KafkaProducerImpl<K extends Serializable, V extends SpecificRecordBase> implements KafkaProducer<K, V> {
+
+    private final KafkaTemplate<K, V> kafkaTemplate;
+
+    public KafkaProducerImpl(KafkaTemplate<K, V> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @Override
+    public void send(String topicName, K key, V message, BiConsumer<SendResult<K, V>, Throwable> callback) {
+        log.info("Sending record to topic {} with key {} and message {}", topicName, key, message);
+        try {
+            CompletableFuture<SendResult<K, V>> kafkaResultFuture = kafkaTemplate.send(topicName, key, message);
+            kafkaResultFuture.whenComplete(callback);
+        }catch (KafkaException kafkaException) {
+            log.error("Error while sending record to topic {} with key {} and message {}",
+                    topicName, key, message, kafkaException.getMessage());
+            throw new KafkaProducerException("Error on kafka producer with key "+ key +" and message " + message);
+        }
+
+    }
+}
